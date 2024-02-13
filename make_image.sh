8905d1572755e982f7f188e228b9b773b3e62a82
danny <danny.meyer@gmail.com>
2024-02-12 21:12:02 -0500

    whoever built the ye site seems to have actually tested this.

--- make_image.sh
+++ make_image.sh
@@ -1,4 +1,4 @@
 random_seed=$RANDOM
 
 # Use the random seed in your ImageMagick command
-magick -size 320x1800 -seed $random_seed plasma:fractal image.png
+magick -size 320x1173 -seed $random_seed plasma:fractal image.png
