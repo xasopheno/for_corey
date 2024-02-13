cf938bee25bba4a24688ffd5471cb18467ab32b9
danny <danny.meyer@gmail.com>
2024-02-12 21:03:20 -0500

    larger?

--- make_image.sh
+++ make_image.sh
@@ -1,1 +1,4 @@
-magick -size 320x3000 plasma:fractal image.png
+random_seed=$RANDOM
+
+# Use the random seed in your ImageMagick command
+magick -size 320x3000 -seed $random_seed plasma:fractal image.png
